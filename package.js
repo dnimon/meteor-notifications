81f9dda1fd16d268bf962ddd53bd2a5b85bccd25
unknown <dnimon@mentortek.com>
2016-02-03 14:10:28 -0600

    Bug fix.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name         : 'dnimon:notifications',
-	version      : '0.1.7',
+	version      : '0.1.8',
 	// Brief, one-line summary of the package.
 	summary      : 'Simple custom notifications for Meteor app',
 	// URL to the Git repository containing the source code for this package.
