5fe2d618c1e5e792114f119e15b4d0acc84c6b4f
unknown <dnimon@mentortek.com>
2016-02-03 10:58:16 -0600

    Added changes to support seeing read notifications

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-	name         : 'miro:notifications',
-	version      : '0.1.0',
+	name         : 'dnimon:notifications',
+	version      : '0.1.1',
 	// Brief, one-line summary of the package.
 	summary      : 'Simple custom notifications for Meteor app',
 	// URL to the Git repository containing the source code for this package.
-	git          : 'https://github.com/MiroHibler/meteor-notifications.git',
+	git          : 'https://github.com/dnimon/meteor-notifications.git',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'
